2e75aa91613c585becf8bcde54b76686ea793d9b
Daniel <daniru9@gmail.com>
2020-09-24 03:32:40 +0200

    añadiendo cartelera a la tab 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: '1c1d39723482b37d957dff324cfc20db',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: '1c1d39723482b37d957dff324cfc20db',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
 
 /*
